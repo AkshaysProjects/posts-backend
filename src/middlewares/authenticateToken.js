be4776ce1ca3b18f23e5616d3adcb468041ad430
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2024-02-08 15:07:18 +0530

    Add middleware to validate access token from authentication header

--- src/middlewares/authenticateToken.js
+++ src/middlewares/authenticateToken.js
@@ -4,6 +4,25 @@ const dotenv = require("dotenv");
 // Initialize dotenv to load environment variables from.env file
 dotenv.config();
 
+const authenticateToken = (req, res, next) => {
+  // Accept the token from the request header
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) {
+    return res.sendStatus(401); // 401 Unauthorized
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.sendStatus(403); // 403 Forbidden
+    }
+
+    req.user = user;
+    next(); // Proceed to the next middleware or route handler
+  });
+};
+
 const authenticateRefreshToken = (req, res, next) => {
   // Fetch the refresh token from the request body
   const refreshToken = req.body.refresh_token;
@@ -22,4 +41,4 @@ const authenticateRefreshToken = (req, res, next) => {
   });
 };
 
-module.exports = { authenticateRefreshToken };
+module.exports = { authenticateToken, authenticateRefreshToken };
