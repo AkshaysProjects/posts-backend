673d0201f2a9b26422ca1ef5d9bdc6efd60ad426
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2024-02-08 15:19:33 +0530

    Add protected posts endpoints

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authRoutes = require("./routes/authRoutes");
 const postRoutes = require("./routes/postRoutes");
+const { authenticateToken } = require("./middlewares/authenticateToken");
 
 // Initialize express application
 const app = express();
@@ -16,6 +17,9 @@ app.get("/", (req, res) => {
 // Route configuration
 app.use("/auth", authRoutes);
 app.use("/posts", postRoutes);
+
+// Route configuration for protected routes
+app.use("/protected/posts", authenticateToken, postRoutes);
 
 // Start the server
 const port = process.env.PORT || 3000;
